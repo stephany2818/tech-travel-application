bb7d6bcbdc64e36b305315634fbfe06c418d0dcc
stephany2818 <stephany2818@gmail.com>
2022-05-30 20:57:46 -0500

    cambios en el home para el contexto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import CartContext from '../../context/cart';
 import { Container, List, Unit } from './styles';
 import api from '../../services/api';
 
 function Home() {
   const [travelsList, setTravelList] = useState([]);
+  const { state, setState } = useContext(CartContext);
+
   useEffect(() => {
     async function getTravelList() {
       const { data } = await api.get('/travels');
@@ -13,6 +16,15 @@ function Home() {
     }
     getTravelList();
   }, []);
+
+  function handlerAddToCart(travel) {
+    const copyCity = [...state.cart];
+    copyCity.push(travel);
+    setState({
+      cart: copyCity,
+    });
+  }
+
   return (
     <Container>
       <List>
@@ -21,7 +33,7 @@ function Home() {
             <img src={city.photo} alt="Travel" />
             <p>{city.title}</p>
             <strong>{city.price}</strong>
-            <button type="button">
+            <button type="button" onClick={() => handlerAddToCart(city)}>
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />
               </div>
